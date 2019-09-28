5c41e99a5f1c56d4aad2c08486ec9fdb1fb3681d
Italo Menezes <italomlporoca@hotmail.com>
2019-09-28 11:51:21 -0300

    Make persistor and save profile from signin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -10,17 +10,19 @@ import history from './services/history';
 import GlobalStyles from './styles/global';
 import { MyThemeProvider } from './styles/ThemeContext';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <MyThemeProvider>
-        <GlobalStyles />
-        <Router history={history}>
-          <Routes />
-        </Router>
-      </MyThemeProvider>
+      <PersistGate persistor={persistor}>
+        <MyThemeProvider>
+          <GlobalStyles />
+          <Router history={history}>
+            <Routes />
+          </Router>
+        </MyThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
---
+++ src/store/modules/user/actions.js
@@ -0,0 +1,10 @@
+// const PREFIX = '@user';
+
+export const TYPES = {};
+
+// export function signInRequest(email, password) {
+//   return {
+//     type: TYPES.signInRequest,
+//     payload: { email, password },
+//   };
+// }
